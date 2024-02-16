ae0c9343fc4637c35fb8a06e56eb392e0a9ccbcb
Strugbits <ezhar@strugbitsglobal.comezhar@strugbitsglobal.comgit config --global user.email ezhar@strugbitsglobal.com>
2024-02-16 21:24:56 +0500

    fix

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import {API} from "../../utilis/services";
 
 // Create async thunks
 export const GetUserById = createAsyncThunk("users/profiles", async () => {
