4f5112237c956ba9b66fa761a8485da42f930b7d
Strugbits <ezhar@strugbitsglobal.comezhar@strugbitsglobal.comgit config --global user.email ezhar@strugbitsglobal.com>
2024-02-28 21:05:43 +0500

    apply wix cms data in homepage

--- src/components/homePageSections/HeroSection.js
+++ src/components/homePageSections/HeroSection.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import img6 from "../../utilis/images/lib/06_desktop.jpg"
-const HeroSection = () => {
+import img6 from "../../utilis/images/lib/06_desktop.jpg";
+
+const HeroSection = ({items}) => {
+  const img = items.map(item => item.data.topimage)
+  console.log(img,"data here");
   return (
     <section className="home-intro" data-aos="d:loop">
-    <div className="container-img">
-      <img
-        src={img6}
-        data-preload
-        className="media"
-        alt=""
-        data-parallax-top
-        data-translate-y="50%"
-      />
-    </div>
-    <p className="fs--14 font-2 blue-1">
-      © Blueprint studios. All rights reserved.
-    </p>
-  </section>
-  
-  )
-}
+      <div className="container-img">
+        <img
+          src={img6}
+          data-preload
+          className="media"
+          alt=""
+          data-parallax-top
+          data-translate-y="50%"
+        />
+      </div>
+      <p className="fs--14 font-2 blue-1">
+        © Blueprint studios. All rights reserved.
+      </p>
+    </section>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
