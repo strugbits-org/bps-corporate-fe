2a9804a3c9d6739620f8439000e3ebe737d4f23a
Strugbits <ezhar@strugbitsglobal.comezhar@strugbitsglobal.comgit config --global user.email ezhar@strugbitsglobal.com>
2024-03-05 20:52:53 +0500

    changes in contact us page and video function

--- src/common/common_functions/videoURL.js
+++ src/common/common_functions/videoURL.js
@@ -1,7 +1,7 @@
 function getFullVideoURL(videoSRC) {
     if (videoSRC && videoSRC.startsWith("wix:video://v1/")) {
         const videoID = videoSRC.replace('wix:video://v1/', '').split('/')[0];
-        return `https://video.wixstatic.com/${videoID}/video.mp4`;
+        return `https://video.wixstatic.com/video/${videoID}/file`;
     } else {
         return videoSRC;
     }
