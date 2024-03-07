9b428c765be3b283fcfef34b24d9a6af8ed84494
osyed002 <osyed@strugbitsglobal.com>
2024-03-07 21:36:34 +0500

    animations issue fix --in progress

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,18 +4,32 @@ import OurFamily from '../components/aboutPageSections/OurFamily';
 import SliderBanner from '../components/commonComponents/SliderBanner';
 import AboutBottomSection from '../components/aboutPageSections/AboutBottomSection';
 import SocialSection from '../components/commonComponents/SocialSection';
+import { useEffect, useState } from 'react';
 
 const AboutUs = () => {
  
+    // Load animation when all sections are loaded
+    const [loadedSections, setLoadedSections] = useState([])
+
+    const numberOfSections = 5;
+    const handleLoadingFinished = () => {
+      setLoadedSections([...loadedSections, true]);
+    }
+    useEffect(() => {
+      console.log("about us loadedSections",loadedSections);
+      if (loadedSections.length === numberOfSections) {
+        document.querySelector(".initScript").click();
+      }
+    }, [loadedSections])
 
   return (
     <>  
-    <IntroSection/>
-    <OurDream/>
-    <OurFamily/>
-    <SliderBanner />
-    <AboutBottomSection/>
-    <SocialSection/>
+    <IntroSection handleLoadingFinished={handleLoadingFinished} />
+    <OurDream handleLoadingFinished={handleLoadingFinished} />
+    <OurFamily handleLoadingFinished={handleLoadingFinished} />
+    <SliderBanner  handleLoadingFinished={handleLoadingFinished} />
+    <AboutBottomSection handleLoadingFinished={handleLoadingFinished} />
+    <SocialSection />
     </>
   )
 }
